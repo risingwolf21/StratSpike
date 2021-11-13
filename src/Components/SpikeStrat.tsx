0a7c2b9e1ea51e6bcf6d55423844875b8bfd456f
Nicolas Albanel <nicolas@callinganyone.com>
2021-11-13 21:05:24 +0000

    positioning last version + clean

--- src/Components/SpikeStrat.tsx
+++ src/Components/SpikeStrat.tsx
@@ -1,100 +1,92 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Draggable from 'react-draggable';
 import './spike.css';
 import tshirt from '../Assets/tshirt.png'
 import tshirt2 from '../Assets/tshirt1.png'
 import spikeball from '../Assets/spikeball.png'
-import { useReactMediaRecorder } from 'react-media-recorder';
 
 const SpikeStrat = () => {
-    const [size, setSize] = useState(50);
-    const [shirt1Pos, setShirt1Pos] = useState<any>([]);
-    const [shirt2Pos, setShirt2Pos] = useState<any>([]);
-    const [shirt3Pos, setShirt3Pos] = useState<any>([]);
-    const [shirt4Pos, setShirt4Pos] = useState<any>([]);
+    const [size, setSize] = useState(1);
+    const forceUpdate = React.useReducer(() => ({}), {})[1] as () => void
 
-    // const [roundnetY, setRoundnetY] = useState<number>();
-
-    useEffect(() => {
+    const updatePosition = useCallback(() => {
         const roundnet = document.getElementById("roundnet");
-        const shirt = document.getElementById("shirt1");
         const shirt1 = document.getElementById("shirt1")?.style;
         const shirt2 = document.getElementById("shirt2")?.style;
         const shirt3 = document.getElementById("shirt3")?.style;
         const shirt4 = document.getElementById("shirt4")?.style;
         const ball = document.getElementById("ball")?.style;
 
-        if (roundnet && shirt1 && shirt2 && shirt3 && shirt4 && ball && shirt) {
+        if (roundnet && shirt1 && shirt2 && shirt3 && shirt4 && ball) {
             const r_rect = roundnet.getBoundingClientRect();
             const x = r_rect.x + (r_rect.width / 2); 
 
             const y = r_rect.y + (r_rect.height / 2);
 
-            const s_rect = shirt.getBoundingClientRect();
-            const shirt_width_center = s_rect.width / 2; 
+            const shirt_width_center = (50 * size) / 2;
 
-            const shirt_height_center = s_rect.height / 2;
+            const shirt_height_center = (50 * size) / 2;
 
-            // setShirt1Pos([
-            //     {
-            //         x: (x - r_rect.width / 2 - 30).toString(),
-            //         y: y.toString()
-            //     }
-            // ])
-            // console.log(shirt1Pos)
-
-            // setShirt2Pos([
-            //     {
-            //         x: (x - r_rect.width / 2 - 30).toString(),
-            //         y: y.toString()
-            //     }
-            // ])
-            shirt1.left = `${(x - r_rect.width / 2 - 70 - shirt_width_center).toString()}px`;
+            shirt1.left = `${(x - r_rect.width / 2 - 80 * size - shirt_width_center).toString()}px`;
             shirt1.top = `${(y - shirt_height_center).toString()}px`;
 
             shirt2.left = `${(x - shirt_width_center).toString()}px`;
-            shirt2.top = `${(y - r_rect.height / 2 - 70 - shirt_height_center).toString()}px`;
+            shirt2.top = `${(y - r_rect.height / 2 - 80 * size - shirt_height_center).toString()}px`;
 
-            shirt3.left = `${(x + r_rect.width / 2 + 70 - shirt_width_center).toString()}px`;
+            shirt3.left = `${(x + r_rect.width / 2 + 80 * size - shirt_width_center).toString()}px`;
             shirt3.top = `${(y - shirt_height_center).toString()}px`;
 
-            shirt4.left = `${(x - shirt_height_center).toString()}px`;
-            shirt4.top = `${(y + r_rect.height / 2 + 70 - shirt_width_center).toString()}px`;
+            shirt4.left = `${(x - shirt_width_center).toString()}px`;
+            shirt4.top = `${(y + r_rect.height / 2 + 80 * size - shirt_height_center).toString()}px`;
 
             ball.left = `${(x + 30).toString()}px`;
-            ball.top = `${(y - r_rect.height / 2 - 70).toString()}px`;
+            ball.top = `${(y - r_rect.height / 2 - 80 * size).toString()}px`;
         }
-    }, [shirt1Pos])
+        
+    }, [size])
+
+    useEffect(() => {
+        updatePosition()
+    }, [updatePosition])
 
 
     return (
         <>
             <div className="buttonContainer">
                 <div>
-                    <button className="sizeButton" onClick={() => setSize(size - 10)}>
+                    <button className="sizeButton" onClick={() => {
+                        setSize(size - 0.1)
+                        updatePosition()
+                        forceUpdate()
+                    }}>
                         -
                     </button>
-                    <button className="sizeButton" onClick={() => setSize(size + 10)}>
+                    <button className="sizeButton" onClick={() => {
+                        setSize(size + 0.1)
+                        updatePosition()
+                        forceUpdate()
+                    }}>
                         +
                     </button>
                 </div>
             </div>
-            <div id="roundnet" style={{width: size + 20, height: size + 20}} className="roundnet">
-            </div>
+            <div id="roundnet" style={{width: 70 * size, height: 70 * size}} className="roundnet"/>
+            <div style={{width: 90 * size, height: 90 * size}} className="nhz"/>
+            <div style={{width: 190 * size, height: 190 * size}} className="nhz"/>
             <Draggable>
-                <img id="shirt1" src={tshirt} style={{width: size, position: "absolute"}} draggable="false" alt="team1-tshirt"/>
+                <img id="shirt1" src={tshirt} style={{width: 50 * size, position: "absolute"}} draggable="false" alt="team1-tshirt"/>
             </Draggable>
             <Draggable>
-                    <img id="shirt2" src={tshirt} style={{width: size, position: "absolute"}} draggable="false" alt="team1-tshirt"/>
+                    <img id="shirt2" src={tshirt} style={{width: 50 * size, position: "absolute"}} draggable="false" alt="team1-tshirt"/>
             </Draggable>
             <Draggable>
-                    <img id="shirt3" src={tshirt2} style={{width: size, position: "absolute"}} draggable="false" alt="team2-tshirt"/>
+                    <img id="shirt3" src={tshirt2} style={{width: 50 * size, position: "absolute"}} draggable="false" alt="team2-tshirt"/>
             </Draggable>
             <Draggable>
-                    <img id="shirt4" src={tshirt2} style={{width: size, position: "absolute"}} draggable="false" alt="team2-tshirt"/>
+                    <img id="shirt4" src={tshirt2} style={{width: 50 * size, position: "absolute"}} draggable="false" alt="team2-tshirt"/>
             </Draggable>
             <Draggable>
-                    <img id="ball" src={spikeball} style={{width: size > 55 ? size - 40 : 15, position: "absolute"}} draggable="false" alt="spikeball-ball"/>
+                    <img id="ball" src={spikeball} style={{width: 15 * size, position: "absolute"}} draggable="false" alt="spikeball-ball"/>
             </Draggable>
         </>
     )
