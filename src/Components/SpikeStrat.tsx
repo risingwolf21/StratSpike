49fe82678ccf74be89656c57e95f45a5aa800559
Nicolas Albanel <nicolas@callinganyone.com>
2021-11-13 21:26:21 +0000

    positioning last version 2

--- src/Components/SpikeStrat.tsx
+++ src/Components/SpikeStrat.tsx
@@ -5,8 +5,54 @@ import tshirt from '../Assets/tshirt.png'
 import tshirt2 from '../Assets/tshirt1.png'
 import spikeball from '../Assets/spikeball.png'
 
+
+const getSize = () => {
+    const width = window.innerWidth
+
+    if (width < 300) {
+        return 0.9
+    } else if (width > 300 && width < 400) {
+        return 1
+    } else if (width > 400 && width < 500) {
+        return 1.1
+    } else if (width > 500 && width < 600) {
+        return 1.2
+    } else if (width > 600 && width < 700) {
+        return 1.3
+    } else if (width > 700 && width < 800) {
+        return 1.4
+    } else if (width > 800 && width < 900) {
+        return 1.5
+    } else if (width > 900 && width < 1000) {
+        return 1.6
+    } else if (width > 1000 && width < 1100) {
+        return 1.7
+    } else if (width > 1100 && width < 1200) {
+        return 1.8
+    } else if (width > 1200 && width < 1300) {
+        return 1.9
+    } else if (width > 1300 && width < 1400) {
+        return 2
+    } else if (width > 1400 && width < 1500) {
+        return 2.1
+    } else if (width > 1500 && width < 1600) {
+        return 2.2
+    } else if (width > 1600 && width < 1700) {
+        return 2.3
+    } else if (width > 1700 && width < 1800) {
+        return 2.4
+    } else if (width > 1800 && width < 1900) {
+        return 2.5
+    } else if (width > 1900 && width < 2000) {
+        return 2.6
+    } else {
+        return 2.7
+    }
+
+}
+
 const SpikeStrat = () => {
-    const [size, setSize] = useState(1);
+    const [size, setSize] = useState(getSize());
     const forceUpdate = React.useReducer(() => ({}), {})[1] as () => void
 
     const updatePosition = useCallback(() => {
