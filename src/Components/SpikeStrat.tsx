95a54183c8371bebbea3868a7b67db5ab0eca2ce
Nicolas Albanel <nicolas@callinganyone.com>
2021-11-14 18:36:58 +0000

    animation

--- src/Components/SpikeStrat.tsx
+++ src/Components/SpikeStrat.tsx
@@ -1,10 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import Draggable from 'react-draggable';
 import './spike.css';
+
+import React, { useCallback, useEffect, useState } from 'react';
+
+import Draggable from 'react-draggable';
+import spikeball from '../Assets/spikeball.png'
 import tshirt from '../Assets/tshirt.png'
 import tshirt2 from '../Assets/tshirt1.png'
-import spikeball from '../Assets/spikeball.png'
-
 
 const getSize = () => {
     const width = window.innerWidth
@@ -53,6 +54,9 @@ const getSize = () => {
 
 const SpikeStrat = () => {
     const [size, setSize] = useState(getSize());
+    const [isRecording, setIsRecording] = useState<boolean>(false);
+    const [step, setStep] = useState<number>(0);
+    const [animation, setAnimation] = useState<any[]>([]);
     const forceUpdate = React.useReducer(() => ({}), {})[1] as () => void
 
     const updatePosition = useCallback(() => {
@@ -95,19 +99,83 @@ const SpikeStrat = () => {
         updatePosition()
     }, [updatePosition])
 
+    const handleDrag = (id: string) => {
+        if (isRecording) {
+            const transform = document.getElementById(id)?.style.transform;
+
+            if (animation.length === step) {
+                setAnimation([...animation, {
+                    id,
+                    data: [
+                        transform
+                    ]
+                }])
+            } else {
+                animation[step].data.push(transform)
+            }
+        }
+    }
+
+    const handleStop = () => {
+        if (isRecording) {
+            setStep(step + 1);
+        }
+    }
+
+    const resetShirt = () => {
+        const shirt1 = document.getElementById("shirt1")?.style;
+        const shirt2 = document.getElementById("shirt2")?.style;
+        const shirt3 = document.getElementById("shirt3")?.style;
+        const shirt4 = document.getElementById("shirt4")?.style;
+        const ball = document.getElementById("ball")?.style;
+        if (shirt1 && shirt2 && shirt3 && shirt4 && ball) {
+            shirt1.transform = "translate(0px, 0px)"
+            shirt2.transform = "translate(0px, 0px)"
+            shirt3.transform = "translate(0px, 0px)"
+            shirt4.transform = "translate(0px, 0px)"
+            ball.transform = "translate(0px, 0px)"
+        }
+    }
+
+    const viewAnimation = async () => {
+        let i = 0
+        let i2 = 0
+
+        resetShirt()
+        let timer = requestAnimationFrame(function animate(timestamp) {
+    
+            const style = document.getElementById(animation[i].id)?.style;
+            if (style) {
+                style.transform = animation[i].data[i2]
+            }
+            
+            if (i2 < animation[i].data.length) {
+                i2++;
+                requestAnimationFrame(animate);
+            } else if (i + 1 < animation.length) {
+                i++;
+                i2 = 0;
+                requestAnimationFrame(animate);
+            } else {
+                return;
+            }
+        });
+    }
 
     return (
         <>
             <div className="buttonContainer">
                 <div>
-                    <button className="sizeButton" onClick={() => {
-                        setSize(size - 0.1)
-                        updatePosition()
-                        forceUpdate()
+                    <button style={{fontSize: 17 * size}} className="sizeButton" onClick={() => {
+                        if (size > 0.8) {
+                            setSize(size - 0.1)
+                            updatePosition()
+                            forceUpdate()
+                        }
                     }}>
                         -
                     </button>
-                    <button className="sizeButton" onClick={() => {
+                    <button style={{fontSize: 17 * size, marginLeft: 3}} className="sizeButton" onClick={() => {
                         setSize(size + 0.1)
                         updatePosition()
                         forceUpdate()
@@ -119,21 +187,60 @@ const SpikeStrat = () => {
             <div id="roundnet" style={{width: 70 * size, height: 70 * size}} className="roundnet"/>
             <div style={{width: 90 * size, height: 90 * size}} className="nhz"/>
             <div style={{width: 190 * size, height: 190 * size}} className="nhz"/>
-            <Draggable>
+            <Draggable
+                onDrag={() => handleDrag("shirt1")}
+                onStop={handleStop}
+            >
                 <img id="shirt1" src={tshirt} style={{width: 50 * size, position: "absolute"}} draggable="false" alt="team1-tshirt"/>
             </Draggable>
-            <Draggable>
+            <Draggable
+                onDrag={() => handleDrag("shirt2")}
+                onStop={handleStop}
+            >
                     <img id="shirt2" src={tshirt} style={{width: 50 * size, position: "absolute"}} draggable="false" alt="team1-tshirt"/>
             </Draggable>
-            <Draggable>
+            <Draggable
+                onDrag={() => handleDrag("shirt3")}
+                onStop={handleStop}
+            >
                     <img id="shirt3" src={tshirt2} style={{width: 50 * size, position: "absolute"}} draggable="false" alt="team2-tshirt"/>
             </Draggable>
-            <Draggable>
+            <Draggable
+                onDrag={() => handleDrag("shirt4")}
+                onStop={handleStop}
+            >
                     <img id="shirt4" src={tshirt2} style={{width: 50 * size, position: "absolute"}} draggable="false" alt="team2-tshirt"/>
             </Draggable>
-            <Draggable>
+            <Draggable
+                onDrag={() => handleDrag("ball")}
+                onStop={handleStop}
+            >
                     <img id="ball" src={spikeball} style={{width: 15 * size, position: "absolute"}} draggable="false" alt="spikeball-ball"/>
             </Draggable>
+            <button style={{fontSize: 17 * size, position: 'absolute', left: 5, bottom: 5}} className="sizeButton" onClick={() => {
+                       if (isRecording) {
+                        setIsRecording(false)
+                       } else {
+                           setIsRecording(true)
+                       }
+                    }}>
+                        {isRecording ? "Stop Recording" : "Record"}
+            </button>
+            {!isRecording && animation.length > 0 && (
+                <button style={{fontSize: 17 * size, position: 'absolute', right: 5, bottom: 5}} className="sizeButton" onClick={viewAnimation}
+                >
+                            View Animation
+                </button>
+            )}
+            {!isRecording && animation.length > 0 && (
+                <button style={{fontSize: 17 * size, position: 'absolute', right: 5, top: 5}} className="sizeButton" onClick={() => {
+                    setAnimation([]);
+                    resetShirt();
+                }}
+                >
+                            Reset Animation
+                </button>
+            )}
         </>
     )
 }
